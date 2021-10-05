eca1b5f3f001e4eb8e90493613001abbc0164a73
Ninnjah <ninnjah.mail@gmail.com>
2021-10-05 23:45:57 +0300

    Add support .mp3

--- utils/player.py
+++ utils/player.py
@@ -1,11 +1,41 @@
+from pathlib import Path
+from typing import List
+
+import numpy as np
+from numpy.typing import NDArray
+from pydub import AudioSegment
 import sounddevice as sd
-import soundfile as sf
+
+
+class Loader:
+    @staticmethod
+    def audio_from_file(filename: Path):
+        suffix: str = filename.suffix.replace(".", "")
+
+        if suffix not in ["mp3", "wav"]:
+            raise ValueError("File doesn't support. This moment player supports only .mp3 .wav files")
+
+        audio: AudioSegment = AudioSegment.from_file(
+            filename.absolute(),
+            format=suffix
+        )
+        audio_bytes: NDArray = np.array(audio.get_array_of_samples())
+
+        return audio_bytes, audio.frame_rate
 
 
 class Player:
+    playlist: List[str]
 
-    def play_sound(self, filename: str) -> None:
-        data, fs = sf.read(filename)
-        print(data, fs)
-        sd.play(data, fs)
-        sd.wait()
+    def __init__(self):
+        self.playlist = list()
+
+    def play_sound(self, filename: Path) -> None:
+        try:
+            data, fs = Loader.audio_from_file(filename)
+        except ValueError:
+            return
+        else:
+            sd.play(data, fs)
+            sd.wait()
+
