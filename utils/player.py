ae206904feeafb91fef0a097732d4fc3f4e754fd
Ninnjah <ninnjah.mail@gmail.com>
2021-10-06 01:08:55 +0300

    Moved to pygame mixer

--- utils/player.py
+++ utils/player.py
@@ -1,41 +1,31 @@
 from pathlib import Path
 from typing import List
 
-import numpy as np
-from numpy.typing import NDArray
-from pydub import AudioSegment
-import sounddevice as sd
-
-
-class Loader:
-    @staticmethod
-    def audio_from_file(filename: Path):
-        suffix: str = filename.suffix.replace(".", "")
-
-        if suffix not in ["mp3", "wav"]:
-            raise ValueError("File doesn't support. This moment player supports only .mp3 .wav files")
-
-        audio: AudioSegment = AudioSegment.from_file(
-            filename.absolute(),
-            format=suffix
-        )
-        audio_bytes: NDArray = np.array(audio.get_array_of_samples())
-
-        return audio_bytes, audio.frame_rate
+import pygame
+from pygame import mixer, event
 
 
 class Player:
     playlist: List[str]
+    player = mixer.music
 
     def __init__(self):
         self.playlist = list()
+        mixer.init(44100)
+        pygame.init()
 
-    def play_sound(self, filename: Path) -> None:
-        try:
-            data, fs = Loader.audio_from_file(filename)
-        except ValueError:
-            return
-        else:
-            sd.play(data, fs)
-            sd.wait()
+    def play_sound(self, filename: str) -> None:
+        self.player.unload()
+        self.player.load(filename)
+        self.player.play()
+        for e in event.get():
+            print(type(e))
 
+    def stop_sound(self):
+        self.player.stop()
+
+    def pause_sound(self):
+        self.player.pause()
+
+    def resume_sound(self):
+        self.player.unpause()
