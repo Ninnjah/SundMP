3c443defe18f54391c886a79eb76d91c3b46c077
Ninnjah <ninnjah.mail@gmail.com>
2021-10-08 19:00:57 +0300

    Add UI

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 import sys
-from time import sleep
+from os import listdir
+from pathlib import Path
+from typing import List
 
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import QFile, QTextStream
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QFileDialog
 
 from ui import player_ui
 from utils.player import Player
 
 
 class PlayerUI(QtWidgets.QMainWindow, player_ui.Ui_MainWindow):
+    playlist: List[str]
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+
+        self.button_play.clicked.connect(self.play_pause_sound)
+        self.button_next.clicked.connect(self.next_sound)
+        self.button_prev.clicked.connect(self.prev_sound)
+        self.action_open_folder.triggered.connect(self.open_folder)
+
+    def open_folder(self):
+        """Opens folder with music"""
+        pass
+
+    def play_pause_sound(self):
+        """Pause or play sound"""
+        pass
+
+    def next_sound(self):
+        """Plays next sound"""
+        pass
+
+    def prev_sound(self):
+        """Plays previous sound"""
+        pass
 
     def closeEvent(self, event):
         pass
@@ -26,12 +50,3 @@ if __name__ == "__main__":
     player_window.show()
 
     sys.exit(app.exec())
-    # for song in os.listdir("test"):
-    #     try:
-    #         player.play_sound(os.path.join("test", song))
-    #         while player.is_playing():
-    #             print(f"\r{player.get_pos()}/{player.get_length()} sec.", end="")
-    #             sleep(0.01)
-    #             pass
-    #     except ValueError:
-    #         pass
