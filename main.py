4e4f8dfda8d3bab5cfe0e2914b2d317eae79f990
Ninnjah <ninnjah.mail@gmail.com>
2021-10-08 19:41:58 +0300

    Add play/pause method Add play_playlist method

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import sys
+import time
 from os import listdir
 from pathlib import Path
 from typing import List
 
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QFileDialog
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow
+from PyQt5.Qt import QTimer
 
 from ui import player_ui
 from utils.player import Player
 
 
-class PlayerUI(QtWidgets.QMainWindow, player_ui.Ui_MainWindow):
+class PlayerUI(QMainWindow, player_ui.Ui_MainWindow):
     playlist: List[str]
 
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
+        self.playlist = list()
 
         self.button_play.clicked.connect(self.play_pause_sound)
         self.button_next.clicked.connect(self.next_sound)
@@ -27,11 +29,33 @@ class PlayerUI(QtWidgets.QMainWindow, player_ui.Ui_MainWindow):
 
         dir_name: str = QFileDialog.getExistingDirectory(self, 'Select directory')
 
-        self.playlist = [x for x in listdir(Path(dir_name).absolute()) if x.endswith((".mp3", ".wav", ".ogg"))]
+        self.playlist = [
+            Path(dir_name).joinpath(x).absolute().as_posix()
+            for x in listdir(Path(dir_name).absolute()) if x.endswith((".mp3", ".wav", ".ogg"))
+        ]
 
     def play_pause_sound(self):
         """Pause or play sound"""
-        pass
+        if player.is_playing():
+            player.pause_sound()
+        else:
+            try:
+                player.sound
+            except AttributeError:
+                self.play_playlist()
+            else:
+                player.resume_sound()
+
+    def play_playlist(self):
+        if not self.playlist:
+            return
+
+        for song in self.playlist:
+            try:
+                player.play_sound(song)
+
+            except ValueError:
+                continue
 
     def next_sound(self):
         """Plays next sound"""
